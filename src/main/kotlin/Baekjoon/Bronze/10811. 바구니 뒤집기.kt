714d720e0ee85d3ac40004237d89004bc908a8c4
yys7517 <yys7517@naver.com>
2025-08-01 14:53:19 +0900

    [Baekjoon] 10811. 바구니 뒤집기 - Bronze II

---
+++ src/main/kotlin/Baekjoon/Bronze/10811. 바구니 뒤집기.kt
@@ -0,0 +1,25 @@
+package org.example.Baekjoon.Bronze
+
+import java.util.*
+
+fun main() {
+    val sc = Scanner(System.`in`)
+
+    val NM = sc.nextLine().split(" ")
+    val N = NM[0].toInt()
+    val M = NM[1].toInt()
+
+    val arr = IntArray(N) { i -> i+1 }
+
+    repeat(M) {
+        val ij = sc.nextLine().split(" ")
+        val i = ij[0].toInt()
+        val j = ij[1].toInt()
+
+        arr.reverse(i-1, j)
+    }
+
+    arr.forEach{
+        print("$it ")
+    }
+}
