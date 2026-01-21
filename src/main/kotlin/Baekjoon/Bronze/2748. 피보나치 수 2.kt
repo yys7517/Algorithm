b0bfc3366e0949f4b5d6bcea87d8bcd8120f0cc0
yys7517 <yys7517@naver.com>
2026-01-21 10:19:43 +0900

    [Baekjoon] 2748. 피보나치 수 2

---
+++ src/main/kotlin/Baekjoon/Bronze/2748. 피보나치 수 2.kt
@@ -0,0 +1,15 @@
+package org.example.Baekjoon.Bronze
+
+    fun main() {
+        val n = readLine()!!.toInt()
+        val dp = LongArray(n+1)
+
+        dp[0] = 0
+        dp[1] = 1
+
+        for(i in 2..n) {
+            dp[i] = dp[i-2] + dp[i-1]
+        }
+
+        print(dp[n])
+    }
