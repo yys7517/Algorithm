6f2215d19cb12de5d7b36add4b2948de8a3abc6d
yys7517 <yys7517@naver.com>
2026-01-30 09:02:54 +0900

    [Baekjoon] 14940. 쉬운 최단거리

--- src/main/kotlin/Baekjoon/Silver/14940. 쉬운 최단거리.kt
+++ src/main/kotlin/Baekjoon/Silver/14940. 쉬운 최단거리.kt
@@ -1,75 +1,77 @@
+package org.example.Baekjoon.Silver
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
 import java.util.*
-import java.io.*
 
 fun main() {
     val bw = BufferedWriter(OutputStreamWriter(System.`out`))
 
     val (n, m) = readLine()!!.split(" ").map { it.toInt() }
-    val map = Array(n) { IntArray(m) }
-    val visited = Array(n) { BooleanArray(m) }
 
+    val map = Array(n) {
+        val st = StringTokenizer(readLine()!!, " ")
+        IntArray(m) { st.nextToken().toInt() }
+    }
+
+    // 목표 지점을 찾아 출발점으로 지정
     var sx = 0
     var sy = 0
 
-    for (i in 0..n-1 ) {
-        val line = readLine()!!.split(" ").map{ it.toInt() }
-        for( j in 0..m-1 ) {
-            map[i][j] = line[j]
-
+    for(i in 0 until n) {
+        for(j in 0 until m) {
+            // print("${map[i][j]} ")
             if(map[i][j] == 2) {
                 sx = i
                 sy = j
+
+                map[i][j] = 0 // 거리로 초기화하기 위해서
             }
+        }
+        // println()
+    }
+
+    // 목표 지점 2에서부터 거리이면,
+    // 2에서 BFS로 출발해서 각 지점간의 거리를 구하자
+    // sx, sy 에서 출발
+
+    val visited = Array(n) { BooleanArray(m) }
+
+    val queue = ArrayDeque<Pair<Int,Int>>()
+    queue.add( Pair(sx, sy) )
+    visited[sx][sy] = true
+
+    // 상하 좌우 이동
+    val dx = arrayOf(-1,1,0,0)
+    val dy = arrayOf(0,0,-1,1)
+
+    while(queue.isNotEmpty()) {
+        val tmp = queue.poll()
+        val x = tmp.first
+        val y = tmp.second
+
+        for(i in 0 until 4) {
+            val nx = x + dx[i]
+            val ny = y + dy[i]
+
+            if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue
+            if(visited[nx][ny]) continue
+            if(map[nx][ny] == 0) continue   // 갈 수 없는 땅
+
+            map[nx][ny] = map[x][y] + 1     // 이전 지점에서 거리 + 1
+            queue.add( Pair(nx, ny) )
+            visited[nx][ny] = true
         }
     }
 
-    // map에서 0은 갈 수 없는 땅, 1은 갈 수 있는 땅, 2는 목표 지점
-
-    fun bfs() {
-        val queue = LinkedList<Pair<Int, Int>>()
-        queue.add( Pair(sx, sy) )
-        visited[sx][sy] = true
-        map[sx][sy] = 0 // 0으로 만들어놓고 시작
-
-        val dx = arrayOf(-1, 1, 0, 0)
-        val dy = arrayOf(0, 0, -1, 1)
-
-        while(queue.isNotEmpty()) {
-            val tmp = queue.poll()
-
-            val tx = tmp.first
-            val ty = tmp.second
-
-            for(i in 0..3) {
-                val nx = tx + dx[i]
-                val ny = ty + dy[i]
-
-                // map을 벗어나면 스킵
-                if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue
-
-
-                if(map[nx][ny] == 1 && !visited[nx][ny]) {
-                    map[nx][ny] = map[tx][ty] + 1
-                    queue.add(Pair(nx, ny))
-                    visited[nx][ny] = true
-                }
-            }
-        }
-    }
-
-    bfs()
-
-    // 원래 갈 수 없는 땅인 위치는 0을 출력하고, 
-    // 원래 갈 수 있는 땅인 부분 중에서 도달할 수 없는 위치는 -1을 출력한다.
-    for(i in 0..n-1) {
-        for ( j in 0..m-1 ) {
-            if(map[i][j] == 1 && !visited[i][j]) {
-                map[i][j] = -1
-            }
-
-            bw.write(map[i][j].toString() + " ")
+    // 갈 수 있는 땅에서 도달할 수 없는 위치는 -1을 출력
+    for(i in 0 until n) {
+        for(j in 0 until m) {
+            if( map[i][j] > 0 && !visited[i][j] ) map[i][j] = -1
+//            print("${map[i][j]} ")
+            bw.write("${map[i][j]} ")
         }
         bw.write("\n")
+//        println()
     }
 
     bw.flush()
