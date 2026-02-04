5e7539a1449f2c4b001d5e0d9c82a0eb0be0fa0e
yys7517 <yys7517@naver.com>
2026-02-04 16:29:05 +0900

    [BOJ] 10989. 수 정렬하기 3

--- src/main/kotlin/Baekjoon/Bronze/10989. 수 정렬하기 3.kt
+++ src/main/kotlin/Baekjoon/Bronze/10989. 수 정렬하기 3.kt
@@ -6,20 +6,19 @@ fun main() {
     val bw = BufferedWriter(OutputStreamWriter(System.`out`))
 
     val n = readLine()!!.toInt()
-    val arr = IntArray(100001)
+    val arr = IntArray(10001)   // 1 ~ 10000 까지의 자연수들이 주어진다.
 
     repeat(n) {
-        arr[readLine()!!.toInt()] += 1
+        arr[ readLine()!!.toInt() ] += 1    // arr[i] = 자연수 i의 개수
     }
 
-    for(i in 0 until 10001) {
-        if(arr[i] > 0) {
-            for(j in 0 until arr[i]) {
-                bw.write("$i\n")
+    bw.use {
+        for( i in 0 until 10001 ) {     // 오름차순으로 출력
+            if(arr[i] > 0) {             // i가 있다면
+                repeat(arr[i]) {
+                    bw.write("$i \n")
+                }
             }
         }
     }
-
-    bw.flush()
-    bw.close()
 }
