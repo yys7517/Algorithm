03458121269ab99c52ddd5b8b012528c1377850a
yys7517 <yys7517@naver.com>
2026-02-09 22:17:20 +0900

    [BOJ] 1446. 지름길 (다익스트라)

--- src/main/kotlin/Baekjoon/Silver/1446. 지름길.kt
+++ src/main/kotlin/Baekjoon/Silver/1446. 지름길.kt
@@ -5,30 +5,39 @@ import java.util.PriorityQueue
 fun main() {
     val (N, D) = readLine()!!.split(" ").map { it.toInt() }
 
-    val dp = IntArray( D + 1 ) { it }
-    val shortcuts = mutableListOf<Triple<Int, Int, Int>>()
+    val graph = Array(D + 1){ mutableListOf<Pair<Int, Int>>() }
+    for(i in 0 until D) {
+        graph[i].add( i+1 to 1 )     // i에서 i+1 지점으로 가는 거리는 1이다.
+    }
 
     repeat(N) {
         val (start, end, cost) = readLine()!!.split(" ").map { it.toInt() }
 
         // 지름길의 역할을 하는지 확인
         if( cost < end - start && end <= D) {
-            shortcuts.add(Triple(start, end, cost))
+            graph[start].add( Pair(end, cost) )
         }
     }
 
-    for(i in 1..D) {
-        dp[i] = minOf(dp[i], dp[i-1] + 1)
+    val dist = IntArray(D + 1) { Int.MAX_VALUE }
+    val pq = PriorityQueue<Pair<Int, Int>>() { a, b -> a.second - b.second }
 
-        for(shortcut in shortcuts) {
-            val(start, end, cost) = shortcut
+    // 0부터 시작
+    dist[0] = 0
+    pq.add(0 to 0)
 
-            if(end == i) {
-                // 현재 위치가 지름길의 도착지점과 같다면
-                dp[i] = minOf(dp[i], dp[start] + cost)
+    while(pq.isNotEmpty()) {
+        val now = pq.poll().first
+
+        for(nextVertex in graph[now]) {
+            val (next, cost) = nextVertex
+
+            if(dist[next] > dist[now] + cost) {
+                dist[next] = dist[now] + cost
+                pq.add(next to dist[next])
             }
         }
     }
 
-    println(dp[D])
+    println(dist[D])
 }
