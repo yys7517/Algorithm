dc0d502e76de8a0aa1d009fa1dc2714fd864755d
yys7517 <yys7517@naver.com>
2026-04-15 09:13:02 +0900

    [BOJ] RGB거리 리팩토링

--- src/main/java/BOJ/silver/Solution_RGB거리.java
+++ src/main/java/BOJ/silver/Solution_RGB거리.java
@@ -12,14 +12,10 @@ public class Solution_RGB거리 {
         int[][] map = new int[N][3];
         int[][] dp = new int[N][3];
 
-        int min = Integer.MAX_VALUE;
-        int minJ = -1;
-
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             for(int j = 0; j < 3; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-
             }
         }
 
@@ -33,25 +29,16 @@ public class Solution_RGB거리 {
         dp[0][1] = map[0][1];
         dp[0][2] = map[0][2];
 
-        dp[1][0] = Math.min(dp[0][1], dp[0][2]) + map[1][0];
-        dp[1][1] = Math.min(dp[0][0], dp[0][2]) + map[1][1];
-        dp[1][2] = Math.min(dp[0][0], dp[0][1]) + map[1][2];
-
-        for(int i = 2; i < N; i++) {
-            for(int j = 0; j < 3; j++) {
-                dp[i][j] = map[i][j];
-
-                if(j == 0) {
-                    dp[i][j] += Math.min(dp[i-1][1], dp[i-1][2]);
-                } else if( j == 1 ) {
-                    dp[i][j] += Math.min(dp[i-1][0], dp[i-1][2]);
-                } else {
-                    dp[i][j] += Math.min(dp[i-1][0], dp[i-1][1]);
-                }
-            }
+        for(int i = 1; i < N; i++) {
+            dp[i][0] += Math.min(dp[i-1][1], dp[i-1][2]) + map[i][0];
+            dp[i][1] += Math.min(dp[i-1][0], dp[i-1][2]) + map[i][1];
+            dp[i][2] += Math.min(dp[i-1][0], dp[i-1][1]) + map[i][2];
         }
 
-        int answer = Math.min( Math.min(dp[N-1][0] , dp[N-1][1]), dp[N-1][2] );
+        int answer = Math.min(
+                Math.min(dp[N-1][0] , dp[N-1][1]),
+                dp[N-1][2]
+        );
 
         System.out.println(answer);
     }
