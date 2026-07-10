c2f7eccd8ae6fc7e2a362b4519f458faefd3b7ab
yys7517 <yys7517@naver.com>
2026-07-10 21:50:38 +0900

    프로그래머스 리코쳇 로봇 복습

--- src/main/java/Programmers/Level_2/Solution_리코쳇로봇.java
+++ src/main/java/Programmers/Level_2/Solution_리코쳇로봇.java
@@ -4,85 +4,84 @@ import java.util.*;
 
 public class Solution_리코쳇로봇 {
 
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+
     static int[][] map;
-    static int R, C;
-    static int sx, sy, ex, ey;
+
+    static int r, c;
+    static int sr, sc;      // 시작 위치
+    static int er, ec;      // 도착 위치
 
     public int solution(String[] board) {
         int answer = 0;
 
-        // (1) 2차원 배열로 변환
-        R = board.length;
-        C = board[0].length();
+        r = board.length;
+        c = board[0].length();
 
-        map = new int[R][C];
+        map = new int[r][c];
 
-        for(int i = 0; i < R; i++) {
-            for(int j = 0; j < C; j++) {
-                char ch = board[i].charAt(j);
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[r][c];
 
-                if( ch == 'R' ) {
-                    sx = i;
-                    sy = j;
-                }
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[i].length(); j++) {
+                if( board[i].charAt(j) == 'R' ) {
+                    sr = i;
+                    sc = j;
+                    map[i][j] = 0;    // 시작점
 
-                if( ch == 'G' ) {
-                    ex = i;
-                    ey = j;
-                }
+                    q.add(new int[] {i, j});
+                    visited[i][j] = true;
 
-                if( ch == 'D' ) {
+                } else if( board[i].charAt(j) == 'D' ) {
                     map[i][j] = -1;
+                } else if( board[i].charAt(j) == 'G' ) {
+                    er = i;
+                    ec = j; // 도착점
+                    map[i][j] = Integer.MAX_VALUE;
+
                 } else {
-                    map[i][j] = Integer.MAX_VALUE;  // 최단 거리를 갱신해야 하므로, MAX값으로 초기화
+                    map[i][j] = Integer.MAX_VALUE;
                 }
             }
         }
 
-        bfs();
-
-        return map[ex][ey] != Integer.MAX_VALUE ? map[ex][ey] : -1;
-    }
-
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,1,-1};
-
-    static void bfs() {
-        Queue<int[]> q = new LinkedList<>();
-        q.add( new int[] { sx, sy } );
-        map[sx][sy] = 0;
-
-        while( !q.isEmpty() ) {
+        while(!q.isEmpty()) {
             int[] curr = q.poll();
 
+            // 현재 위치
             int x = curr[0];
             int y = curr[1];
 
             for(int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+                int nx = x;
+                int ny = y;
 
-                while( validation(nx, ny) && map[nx][ny] != -1 ) {
+                // 게임판 위의 장애물이나 게임판 가장자리까지 부딪힐 때까지 미끄러져 움직이는 것을 한 번의 이동
+                while(true) {
                     nx += dx[i];
                     ny += dy[i];
+
+                    if( nx < 0 || ny < 0 || nx >= r || ny >= c ) break;
+                    if( map[nx][ny] == -1 ) break;
                 }
 
-                // 장애물이나 벽을 만나면, 반복문이 끝나므로
-                // 이전 단계로 다시 이동시켜줘야 한다.
+                // 장애지점을 만나서 멈춘 것이므로, 한 칸 빠꾸해야함
                 nx -= dx[i];
                 ny -= dy[i];
 
-                if( map[nx][ny] < map[x][y] + 1 ) continue;
+                if( !visited[nx][ny] ) {
+                    // 현재 위치로 오기까지 이동 횟수 갱신
+                    map[nx][ny] = map[x][y] + 1;
 
-                q.add( new int[] { nx, ny } );
-                map[nx][ny] = Math.min( map[nx][ny], map[x][y] + 1 );
+                    q.add(new int[] { nx, ny });
+                    visited[nx][ny] = true;
+                }
+
             }
         }
-    }
 
-
-    static boolean validation( int x, int y ) {
-        if( x < 0 || y < 0 || x >= R || y >= C ) return false;
-        return true;
+        return map[er][ec] == Integer.MAX_VALUE ? -1 : map[er][ec];
     }
 }
