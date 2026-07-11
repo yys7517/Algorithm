35861ef172b98b92be2bf5c960d2a39207555e95
yys7517 <yys7517@naver.com>
2026-07-11 17:52:33 +0900

    프로그래머스 네트워크 복습

--- src/main/java/Programmers/Level_3/Solution_네트워크.java
+++ src/main/java/Programmers/Level_3/Solution_네트워크.java
@@ -1,31 +1,52 @@
 package Programmers.Level_3;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution_네트워크 {
+    static boolean[] visited;
+
     public int solution(int n, int[][] computers) {
         int answer = 0;
 
-        boolean[] visited = new boolean[n];
+        visited = new boolean[n];
 
         for(int i = 0; i < n; i++) {
             // 방문하지 않은 곳 부터 dfs 시작
             if( !visited[i] ) {
                 answer++;
-
-                dfs(i, n, computers, visited);
+                dfs(i, n, computers);
             }
         }
 
         return answer;
     }
 
-    private static void dfs(int curr, int n, int[][] computers, boolean[] visited) {
+    static void dfs(int curr, int n, int[][] computers) {
         visited[curr] = true;
 
         for(int i = 0; i < n; i++) {
             // 방문한 적이 없고, 연결되어 있다면
             if( !visited[i] && computers[curr][i] == 1 ) {
+                dfs(i, n, computers);
+            }
+        }
+    }
 
-                dfs(i, n, computers, visited);
+    static void bfs(int start, int n, int[][] computers) {
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(start);
+        visited[start] = true;
+
+        while(!q.isEmpty()) {
+            int curr = q.poll();
+
+            for(int i = 0; i < n; i++) {
+                if( computers[curr][i] == 1 && !visited[i] ) {  // 연결되어 있지만, 방문하지 않은 정점인가?
+                    q.add(i);
+                    visited[i] = true;
+                }
             }
         }
     }
