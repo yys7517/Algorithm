58d6ff8fae2a8c12c09310696a62efd17739bf4a
yys7517 <yys7517@naver.com>
2026-07-13 15:27:14 +0900

    프로그래머스 경주로 건설 복습

--- src/main/java/Programmers/Level_3/Solution_경주로_건설.java
+++ src/main/java/Programmers/Level_3/Solution_경주로_건설.java
@@ -4,72 +4,71 @@ import java.util.*;
 
 public class Solution_경주로_건설 {
     // 상 하 좌 우
-    static int[] dx = { -1, 1, 0, 0 };
-    static int[] dy = { 0, 0, -1, 1 };
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
     static final int INF = Integer.MAX_VALUE;
 
     public int solution(int[][] board) {
-        int answer = 0;
+        int answer = INF;
+
         int n = board.length;
 
-        // 그 정점에, 어떤 방향으로 도착했는지, 모든 경우의 수, 정점에 도착할 수 있는 최단 거리를 모두 기록
-        int[][][] dist = new int[n][n][4];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                Arrays.fill(dist[i][j], Integer.MAX_VALUE);
+        int[][][] costs = new int[n][n][4];  // 4가지의 방향을 통해, 특정 정점에 도착하기 까지 든 비용
+        for(int i = 0; i < costs.length; i++) {
+            for(int j = 0; j < costs[i].length; j++) {
+                Arrays.fill(costs[i][j], INF);
             }
         }
 
-        // pq의 요소
-        // (정점x, 정점y, 방향, 거리)
-        PriorityQueue<int[]> pq = new PriorityQueue<>( (a, b) -> Integer.compare(a[3], b[3]) );
+        // (정점 x, 정점 y, 방향, 비용)
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[3], b[3]));
 
-        pq.add( new int[] {0,0,1,0} );  // (0,0)에서 아래로 출발
-        pq.add( new int[] {0,0,3,0} );  // (0,0)에서 우측으로 출발
+        // 시작점에서는 우측이랑, 아래 방향으로만 이동 가능
 
-        dist[0][0][1] = 0;
-        dist[0][0][3] = 0;
+        costs[0][0][1] = 0; // 방향 1: 하단
+        costs[0][0][3] = 0; // 방향 3: 우측
 
-        // dx[1], dy[1] - 하단
-        // dx[3], dy[3] - 우측
-        // 출발점에서는, 우측과 하단으로만 출발할 수 있다.
+        pq.add(new int[] {0,0,1,0});
+        pq.add(new int[] {0,0,3,0});
 
         while(!pq.isEmpty()) {
+            // (정점 x, 정점 y, 방향, 비용)
             int[] curr = pq.poll();
+
             int x = curr[0];
             int y = curr[1];
             int dir = curr[2];
             int cost = curr[3];
 
-            // 지금 갱신하려는 값이 최단거리가 아니면 skip
-            if( dist[x][y][dir] < cost ) continue;
+            if( costs[x][y][dir] < cost ) continue;
 
             for(int nextDir = 0; nextDir < 4; nextDir++) {
                 int nx = x + dx[nextDir];
                 int ny = y + dy[nextDir];
 
-                // 이동 가능한지 확인
+                // 이동 가능한지?
                 if( nx < 0 || ny < 0 || nx >= n || ny >= n ) continue;
-                if( board[nx][ny] == 1 ) continue;  //  벽이면 안됨
+                if( board[nx][ny] == 1 ) continue;      // 벽이면 skip
 
-                int nCost = cost + 100;
-                if( dir != nextDir ) nCost += 500;
 
-                if( dist[nx][ny][nextDir] > nCost ) {
-                    dist[nx][ny][nextDir] = nCost;
+                int nCost = cost + 100; // 도로의 길이는 100
+
+                // 코너이면 비용이 500만큼 더 추가 : 이전 방향과 달라지면, 코너가 생긴다.
+                if(dir != nextDir) nCost += 500;
+
+                // 최소 비용 갱신
+                if( costs[nx][ny][nextDir] > nCost ) {
+                    costs[nx][ny][nextDir] = nCost;
                     pq.add(new int[] {nx, ny, nextDir, nCost});
                 }
             }
         }
 
-//         return Math.min(
-//             dist[n-1][n-1][0],
-//             Math.min( dist[n-1][n-1][1],
-//                     Math.min( dist[n-1][n-1][2], dist[n-1][n-1][3] )
-//             )
-//         );
+        for(int i = 0; i < 4; i++) {
+            answer = Math.min(answer, costs[n-1][n-1][i]);
+        }
 
-        return Arrays.stream(dist[n-1][n-1]).min().getAsInt();
+        return answer;
     }
 }
