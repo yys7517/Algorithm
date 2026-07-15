d4727fa8fdf46f6c75bc9d3cea57513538116a79
yys7517 <yys7517@naver.com>
2026-07-15 16:18:35 +0900

    프로그래머스 기능개발 복습

--- src/main/java/Programmers/Level_2/Solution_기능개발.java
+++ src/main/java/Programmers/Level_2/Solution_기능개발.java
@@ -6,58 +6,43 @@ public class Solution_기능개발 {
     public int[] solution(int[] progresses, int[] speeds) {
         int[] answer = {};
 
-        ArrayDeque<Integer> deque = new ArrayDeque<>();
-
-        int[] times = new int[speeds.length];
+        // int[] days = new int[progresses.length];
+        ArrayDeque<Integer> days = new ArrayDeque<>();
 
         for(int i = 0; i < speeds.length; i++) {
-            int remain = 100 - progresses[i];       // 남은 %
-            int time = remain % speeds[i] == 0 ? remain / speeds[i] : remain / speeds[i] + 1;          // 걸리는 시간
-            // 남은 시간이 작업 속도로 나누어질 때랑, 딱 안 나누어질 때랑 나눠야함.
+            int day = (100 - progresses[i]) % speeds[i] == 0 ?
+                    (100 - progresses[i]) / speeds[i]
+                    : ((100 - progresses[i]) / speeds[i]) + 1;
 
-            // System.out.print(remain + " ");
-            // System.out.print(time + " ");
-
-            times[i] = time;
-            deque.addLast(time);
+            // days[i] = day;
+            days.add(day);
         }
 
-        // System.out.println();
-
-        List<Integer> list = new ArrayList<>();
-
-        while( !deque.isEmpty() ) {
-            int currTime = deque.removeFirst();
-            // System.out.print(currTime + " ");
-            int count = 1;
-
-            if( !deque.isEmpty() ) {
-                // System.out.println(deque.peekFirst());
-
-                if( deque.peekFirst() > currTime ) {
-                    // 다음 기능이 개발되기까지 더 남았을 때
-                    list.add(count);
-                } else {
-                    // 다음 기능들이 현재 기능보다 더 빨리 개발되었을 때
-                    while( !deque.isEmpty() && deque.peekFirst() <= currTime ) {
-                        deque.removeFirst();
-                        count++;
-                    }
-
-                    list.add(count);
-                }
-            } else {
-                list.add(count);
-            }
-        }
-
-        // for(int num: list) {
-        //     System.out.print(num + " ");
+        // for(int i = 0; i < days.length; i++) {
+        //     System.out.printf("%d ", days[i]);
         // }
 
+        ArrayList<Integer> answerList = new ArrayList<>();
 
-        return list.stream()
-                .mapToInt(i -> i)
-                .toArray();
+        while( !days.isEmpty() ) {
+            int curr = days.poll();
+            int count = 1;
+
+            // 남은 완료일자가 있는지 && 그 업무가 이미 완료된 업무인지
+            while( !days.isEmpty() && days.peekFirst() <= curr ) {
+                days.removeFirst();
+                count++;
+            }
+
+            answerList.add(count);
+        }
+
+        // List 내용을 Array로 Copy
+        answer = new int[answerList.size()];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = answerList.get(i);
+        }
+
+        return answer;
     }
 }
