66923ab5810110ba6bc6e3e3f30c81719dc3052a
yys7517 <yys7517@naver.com>
2026-07-16 11:07:22 +0900

    프로그래머스 모의고사 복습

--- src/main/java/Programmers/Level_1/Solution_모의고사.java
+++ src/main/java/Programmers/Level_1/Solution_모의고사.java
@@ -1,47 +1,49 @@
 package Programmers.Level_1;
 
+import java.util.ArrayList;
+
 public class Solution_모의고사 {
     public int[] solution(int[] answers) {
-        int[] student1 = {1,2,3,4,5};
-        int[] student2 = {2,1,2,3,2,4,2,5};
-        int[] student3 = {3,3,1,1,2,2,4,4,5,5};
+        int[] answer = {};
 
+        int[] student1 = {1, 2, 3, 4, 5};
+        int[] student2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] student3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-        int[] counts = new int[3];
-        int maxCount = counts[0];
+        int[] count = new int[4];
 
         for(int i = 0; i < answers.length; i++) {
-            if(answers[i] == student1[i % student1.length]) {
-                counts[0]++;
+            if( answers[i] == student1[i % student1.length] ) {
+                count[1]++;
             }
 
-            if(answers[i] == student2[i % student2.length]) {
-                counts[1]++;
+            if( answers[i] == student2[i % student2.length] ) {
+                count[2]++;
             }
 
-            if(answers[i] == student3[i % student3.length]) {
-                counts[2]++;
-            }
-        }
-
-        for(int i = 0; i < 3; i++) {
-            maxCount = Math.max(maxCount, counts[i]);
-        }
-
-        int count = 0;
-        for(int i = 0; i < 3; i++) {
-            if(counts[i] == maxCount) {
-                count++;
+            if( answers[i] == student3[i % student3.length] ) {
+                count[3]++;
             }
         }
 
-        int[] answer = new int[count];
-        int idx = 0;
+        // 가장 많이 맞춘 문제 개수
+        int maxCount = Integer.MIN_VALUE;
+        for(int i = 1; i <= 3; i++) {
+            maxCount = Math.max(maxCount, count[i]);
+        }
 
-        for(int i = 0; i < 3; i++) {
-            if(counts[i] == maxCount) {
-                answer[idx++] = i+1;
+        // 문제를 가장 많이 맞춘 학생 수, 여럿이면 오름차순 return
+        ArrayList<Integer> answerList = new ArrayList<>();
+
+        for(int i = 1; i <= 3; i++) {
+            if(count[i] == maxCount) {
+                answerList.add(i);
             }
+        }
+
+        answer = new int[answerList.size()];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = answerList.get(i);
         }
 
         return answer;
