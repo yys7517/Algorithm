9b18f9fd83026c4b3a0e9bcdc6b7a1f52ed290aa
yys7517 <yys7517@naver.com>
2026-07-16 16:55:56 +0900

    프로그래머스 전력망 둘로 나누기 풀이 최적화, DFS 적용

--- src/main/java/Programmers/Level_2/Solution_전력망둘로나누기.java
+++ src/main/java/Programmers/Level_2/Solution_전력망둘로나누기.java
@@ -1,59 +1,53 @@
 package Programmers.Level_2;
 
-import java.util.*;
+import java.util.ArrayList;
 
 public class Solution_전력망둘로나누기 {
-    ArrayList<ArrayList<Integer>> adj;
+    static ArrayList<ArrayList<Integer>> adj;
+    static int N;
+    static boolean[] visited;
+    static int minDiff;
+
+    static int dfs(int current) {
+        int child = 1;
+
+        for(int next: adj.get(current)) {
+            if( !visited[next] ) {
+                visited[next] = true;
+                child += dfs(next);
+            }
+        }
+
+        // 현재 current에서 DFS를 했을 때, 현재 current를 루트로 하는 서브트리의 크기를 구할 수 있다.
+        // 현재 서브트리 크기, 전체 크기 - 현재 서브트리 크기 는 전력망의 차이와 같다. (전선이 끊어진 것과 같은 원리)
+        int diff = Math.abs(child - (N - child));   // 재귀가 타고 내려가면 갈 수록, 서브트리의 루트 값이 달라지므로, 모든 케이스의 전력망의 차이를 모두 구할 수 있다.
+
+        minDiff = Math.min(minDiff, diff);
+        return child;
+    }
 
     public int solution(int n, int[][] wires) {
-        int answer = Integer.MAX_VALUE;
+        adj = new ArrayList<>();
+        for(int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
+        }
 
-        // 전선 하나 골라서 끊어보자.
-        for(int wireNum = 0; wireNum < wires.length; wireNum++) {
-            adj = new ArrayList<>();
-            for(int i = 0; i <= n; i++) {
-                adj.add(new ArrayList<>());
-            }
+        N = n;
+        visited = new boolean[n+1];
 
-            // 전선 하나 끊은 상태의 인접 그래프 생성
-            for(int i = 0; i < wires.length; i++) {
-                if( i == wireNum ) continue;    // i번째 전선 끊어보기.
+        minDiff = Integer.MAX_VALUE;
 
-                int v1 = wires[i][0];
-                int v2 = wires[i][1];
+        for(int[] wire: wires) {
+            int v1 = wire[0];
+            int v2 = wire[1];
 
-                // v1-v2 연결
-                adj.get(v1).add(v2);
-                adj.get(v2).add(v1);
-            }
+            adj.get(v1).add(v2);
+            adj.get(v2).add(v1);
+        }
 
-            // BFS 돌려보자.
-            Queue<Integer> q = new ArrayDeque<>();
-            boolean[] visited = new boolean[n+1];
+        visited[1] = true;
+        dfs(1);
 
-            // 1번 송전탑에서 출발하자
-            int count = 1;      // BFS 돌려서, 한 번 탐색에서 개수 찾기
-            q.add(1);
-            visited[1] = true;
-
-            while(!q.isEmpty()) {
-                int curr = q.poll();
-
-                for(int next: adj.get(curr)) {
-                    if( !visited[next] ) {
-                        q.add(next);
-                        visited[next] = true;
-                        count++;
-                    }
-                }
-            }
-
-            // 송전탑 개수는 각각
-            // count 개, n - count개
-            answer = Math.min(answer, Math.abs(count - (n - count)));   // 송전탑 차이 최소값
-
-        } // End 전선 하나 끊기
-
-        return answer;
+        return minDiff;
     }
 }
