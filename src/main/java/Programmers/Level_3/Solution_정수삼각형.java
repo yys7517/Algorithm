de70cc9563f777d1281a046a70b1848e73a58b11
yys7517 <yys7517@naver.com>
2026-07-20 14:50:09 +0900

    프로그래머스 정수 삼각형

--- src/main/java/Programmers/Level_3/Solution_정수삼각형.java
+++ src/main/java/Programmers/Level_3/Solution_정수삼각형.java
@@ -1,28 +1,53 @@
 package Programmers.Level_3;
 
 public class Solution_정수삼각형 {
-    public int solution(int[][] triangle) {
-        int answer = 0;
-        int[][] dp = new int[triangle.length][triangle.length];
+    class Solution {
+        public int solution(int[][] triangle) {
+            int answer = 0;
 
-        dp[0][0] = triangle[0][0];
-        dp[1][0] = triangle[0][0] + triangle[1][0];
-        dp[1][1] = triangle[0][0] + triangle[1][1];
+            int row = triangle.length;
+            int col = triangle[row-1].length;
 
-        for(int i = 2; i < triangle.length; i++) {
-            for(int j = 0; j < triangle[i].length; j++) {
-                dp[i][j] += triangle[i][j];
+            int[][] dp = new int[row][col];
+            dp[0][0] = triangle[0][0];
 
-                if( j == 0 ) {
-                    dp[i][j] += dp[i-1][0];
-                } else {
-                    dp[i][j] += Math.max( dp[i-1][j] , dp[i-1][j-1] );
+            // 삼각형의 높이는 1 이상 500 이하입니다.
+            // dp[1][0] = triangle[0][0] + triangle[1][0];
+            // dp[1][1] = triangle[0][0] + triangle[1][1];
+
+//         dp[2][0] = dp[1][0] + triangle[2][0];
+//         dp[2][1] = Math.max(dp[1][0], dp[1][1]) + triangle[2][1];
+//         dp[2][2] = dp[1][1] + triangle[2][2];
+
+//         dp[3][0] = dp[2][0] + triangle[3][0];
+//         dp[3][1] = Math.max(dp[2][0], dp[2][1]) + triangle[3][1];
+//         dp[3][2] = Math.max(dp[2][1], dp[2][2]) + triangle[3][2];
+//         dp[3][3] = dp[2][2] + triangle[3][3];
+
+//         dp[4][0] = dp[3][0] + triangle[4][0];
+//         dp[4][1] = Math.max(dp[3][0], dp[3][1]) + triangle[4][1];
+
+            for(int r = 1; r < row; r++) {
+                for(int c = 0; c <= r; c++) {
+                    dp[r][c] += triangle[r][c];
+
+                    if( c == 0 ) { // 가장 왼쪽
+                        dp[r][c] += dp[r-1][0];
+                    } else if( c == r ) {   // 가장 오른쪽
+                        dp[r][c] += dp[r-1][c-1];
+                    } else { // 가운데
+                        dp[r][c] += Math.max(dp[r-1][c-1], dp[r-1][c]);
+                    }
                 }
-
-                answer = Math.max( answer, dp[i][j] );
             }
-        }
 
-        return answer;
+            answer = -1;
+
+            for(int c = 0; c < col; c++) {
+                answer = Math.max(answer, dp[row-1][c]);
+            }
+
+            return answer;
+        }
     }
 }
