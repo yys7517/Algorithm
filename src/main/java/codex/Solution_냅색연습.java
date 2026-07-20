4a1a75009c0ed78faf644e41c4f008ce7faf707c
yys7517 <yys7517@naver.com>
2026-07-20 16:18:50 +0900

    냅색 알고리즘 복습

--- src/main/java/codex/Solution_냅색연습.java
+++ src/main/java/codex/Solution_냅색연습.java
@@ -18,26 +18,32 @@ public class Solution_냅색연습 {
     }
 
     static int solution(int capacity, int[][] items) {
-        int[][] dp = new int[items.length + 1][capacity+1];   // dp[i][j] = 배낭의 무게가 i일 때, j번째까지 아이템을 확인했을 때 최대 가치
+        int N = items.length;   // 아이템 개수
+        int[][] dp = new int[N+1][capacity+1];  // dp[i][j] = i번째 아이템까지 확인했을 때, 그리고 현재 배낭의 최대 무게가 j일 때, 가질 수 있는 최대 가치
 
-        for(int i = 1; i <= items.length; i++) {
+        for(int i = 1; i <= N; i++) {
             int itemWeight = items[i-1][0];
             int itemValue = items[i-1][1];
 
-            for(int weight = 0; weight <= capacity; weight++) {
-                if( weight >= itemWeight ) {
-                    // 현재 물건을 담는다.
-                    dp[i][weight] = Math.max(
-                            dp[i-1][weight - itemWeight] + itemValue,   // 현재 물건의 무게만큼 빠졌을 때 가치와, 현재 물건의 가치의 합
-                            dp[i-1][weight]         // 이전 물건까지 확인했을 때 최대 가치
-                    );
-                } else {
+            // 현재 배낭 무게 weight
+            for(int weight = 1; weight <= capacity; weight++) {
+                if( weight < itemWeight ) {
+                    // 현재 배낭에 담을 수 있는 최대 무게보다 아이템이 더 무겁다면
                     // 담지 않는다.
-                    dp[i][weight] = dp[i-1][weight];    // 이전 물건까지 확인했을 때, 최대 가질 수 있는 값과 같다.
+                    dp[i][weight] = dp[i-1][weight];    // 이전 아이템까지 확인했을 때, 최대 가질 수 있는 가치와 같다.
+                } else {
+                    // 현재 물건을 담을 수 있다.
+                    // 하지만, 지금까지 담은 물건과 현재 물건을 같이 담을 수도 있기 때문에
+                    // 현재 물건을 담는 경우와
+                    // 담지 않는 경우로 나뉠 수 있다.
+                    dp[i][weight] = Math.max(
+                            dp[i-1][weight - itemWeight] + itemValue,   // 현재 물건을 담는다.
+                            dp[i-1][weight] // 현재 물건을 담지 않는다.
+                    );
                 }
             }
         }
 
-        return dp[items.length][capacity];  // 배낭이 capacity만큼 담을 수 있을 때, 모든 아이템을 확인해서, 최대 가치를 구하라.
+        return dp[N][capacity]; // 모든 아이템을 확인했고, 배낭의 무게가 capacity일 때, 가질 수 있는 최대 가치
     }
 }
