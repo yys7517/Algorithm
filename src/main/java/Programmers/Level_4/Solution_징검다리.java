2821276aef6ccded96502d4e25ef9e34605c30ba
yys7517 <yys7517@naver.com>
2026-07-21 19:25:02 +0900

    프로그래머스 징검다리 복습

--- src/main/java/Programmers/Level_4/Solution_징검다리.java
+++ src/main/java/Programmers/Level_4/Solution_징검다리.java
@@ -6,46 +6,44 @@ public class Solution_징검다리 {
     public int solution(int distance, int[] rocks, int n) {
         int answer = 0;
 
-        // 1. left, right값 설정 - 최소 경우의 수, 최악 경우의 수
-        // 바위를 제거한 후의, 최대 거리를 구하는 것이므로, 거리를 기준으로 설정
-        int left = 0;
-        int right = distance;
-
-        // 2.. 탐색할 배열은 무조건 오름차순 정렬
+        // 이분 탐색의 제일 첫 번째 - 정렬
         Arrays.sort(rocks);
 
+        // 바위 사이의 거리를 몇으로 해야 n개를 제거할 수 있을까?
+        // 바위 사이의 거리, 각 지점 사이의 거리의 최솟값 중에 가장 큰 값을 찾아야 함!
+        int left = 1;   // 최소: 1
+        int right = distance;  // distance
+
         while( left <= right ) {
-            // 3. mid 값 설정 후 탐색 진행
-            int mid = (left + right) / 2;
+            int mid = (left + right) / 2;   // 각 지점 사이의 거리 최솟값
 
-            int prev = 0;
-            int removeCount = 0;
+            int needRemovals = 0;       // 각 지점 사이의 거리 중 최솟값인 mid를 만족하기 위해 제거해야 하는 돌의 갯수
+            int current = 0;
 
-            for(int i = 0; i < rocks.length; i++) {
-                if( rocks[i] - prev >= mid ) {
-                    // 바위 간 거리 최소 값 이상을 만족해??
-                    prev = rocks[i];    // 그럼 이동!!
+            for( int rock: rocks ) {
+                if( rock - current >= mid ) {   // mid보다 커야 이동함.
+                    current = rock;
                 } else {
-                    removeCount++;  // 그럼 이 바위는 쓸모 없으니 제거하자.
+                    // 사이 거리가 mid보다 작으므로, 바위 제거
+                    needRemovals++;
                 }
             }
 
-            // 마지막 바위 지점 - 도착지점
-            if( distance - prev < mid ) {
-                // 마지막 구간이 최소 값 보다 작으면
-                removeCount++; // prev에 위치한 바위도 제거!
+            // 마지막 지점에서 도착 지점까지 이동
+            if( distance - current < mid ) {
+                // mid보다 간격이 좁으면, 바위를 더 제거해야 한다.
+                needRemovals++;
             }
 
-            if( removeCount > n ) {
-                // 너무 많이 제거했어?
-                // mid 값을 줄여서, 바위 제거 개수를 줄이자. mid값보다 크거나 같을 때만 이동시키고 있다.
-                right = mid-1;
+            if( needRemovals > n ) {
+                // 바위 제거하는 개수를 줄이려면, 지점 사이의 거리 최솟값인 mid값을 더 줄여도 됨
+                right = mid - 1;
             } else {
                 // removeCount <= n
-                // 바위 제거 개수가 작거나 같아?
-                // 같으면 정답이 되고!!,
-                answer = mid;
-                left = mid + 1; // 더 적다면, mid값을 늘려보자
+                answer = mid;   // n개의 바위를 제거한 것이라면, 정답이 될 수도 있음.
+
+                // 제거 개수가 n개보다 적다면 ? 바위를 더 제거해도 됨. mid 값을 늘리자. (가능한 mid의 최댓값을 찾아야함.)
+                left = mid + 1;
             }
         }
 
