8f21c4b2c203a65f418bf4a5fc9cef0e1839589b
yys7517 <yys7517@naver.com>
2026-07-21 17:21:52 +0900

    프로그래머스 입국심사 복습

--- src/main/java/Programmers/Level_3/Solution_입국심사.java
+++ src/main/java/Programmers/Level_3/Solution_입국심사.java
@@ -8,25 +8,30 @@ public class Solution_입국심사 {
 
         Arrays.sort(times);
 
-        long left = times[0];    // 최소 = 1명 * 최소시간
-        long right = (long) times[times.length - 1] * n; // 최악 = 최대시간 * n
+        // *** 모든 사람이 심사를 받는데 걸리는 시간을 최소로 하고싶다 ***
+        long left = times[0];   // 최소 : 1명 * 최소 시간
+        long right = (long) times[times.length-1] * n; // 최악 : n명이 제일 오래 걸리는 심사관한테
 
-        while (left <= right) {
-            long mid = (left + right) / 2;   // 이 정도 시간이면 모두 심사가 가능한가?
+        while( left <= right ) {
+            long mid = (left + right) / 2;  // 모든 사람이 심사 받는데 걸리는 시간이라면?
 
+            // mid 시간 동안, 심사관들은 몇 명을 체크인 할 수 있을까?
             long checked = 0L;
-            for (int i = 0; i < times.length; i++) {
-                // 각 심사위원들이 mid 시간동안 check-in할 수 있는 인원 수
-                checked += mid / times[i];
+
+            for(int time: times) {
+                checked += mid / time;
+
+                if( checked >= n ) break;   // 불필요한 연산을 줄일 수 있다.
             }
 
-            if (checked >= n) {
-                answer = mid;   // 정답이 될 수도 있다.
-                right = mid - 1; // 더 적은 시간안에 할 수 있지 않을까?
-            } else {
-                // checked < n
-                // n명을 체크인하려면 더 많은 시간이 필요하다
+            if( checked < n ) {
+                // mid 값이 더 커져야 함.
                 left = mid + 1;
+            } else {
+                // checked >= n
+                // 현재 mid가 정답이거나, mid값을 더 작게 해서, 최솟값을 찾을 수 있음
+                answer = mid;
+                right = mid - 1;
             }
         }
 
