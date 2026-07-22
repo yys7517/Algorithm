26140a48ccc375493521dca09345b792a9999bd6
yys7517 <yys7517@naver.com>
2026-07-22 18:38:57 +0900

    프로그래머스 구명보트 복습

--- src/main/java/Programmers/Level_2/Solution_구명보트.java
+++ src/main/java/Programmers/Level_2/Solution_구명보트.java
@@ -4,32 +4,36 @@ import java.util.*;
 
 public class Solution_구명보트 {
     public int solution(int[] people, int limit) {
-        int answer = 0;
+        int answer = 0; // 모든 사람을 구출하기 위해 필요한 구명보트 갯수
 
+        int n = people.length;  // people[] - 사람들의 무게
+
+        // 무게 기준 오름차순으로 정렬
         Arrays.sort(people);
 
-        // 최대 2명
-        // 무게 제한 limit
-        // left + right
-
+        // 하나의 구명보트에는 최대 2명, 무게 제한 limit
         int left = 0;
-        int right = people.length -1;
+        int right = n-1;
 
-        while( left < right ) {
+        while(left <= right) {
+            // 딱 한 명만 남은 경우
+            if(left == right) {
+                answer++;
+                break;
+            }
+
             if( people[left] + people[right] <= limit ) {
-                answer++;   // 최적의 경우: 2명이서 보트 탑승
                 left++;
                 right--;
+
+                answer++;   // 둘이 같이 타기
             } else {
                 // left + right > limit
-                // 둘의 합이 limit보다 크면, right만 탑승
-                answer++;
-                right--;
-            }
-        }
 
-        if( left == right ) {
-            answer++;
+                // 둘이 같이 못 타니까, 무거운 사람 혼자 타기 (오름차순 정렬)
+                right--;
+                answer++;
+            }
         }
 
         return answer;
