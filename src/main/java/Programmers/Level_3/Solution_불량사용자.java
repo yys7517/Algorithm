46943119138145b0c5703212b17e87939b433bb1
yys7517 <yys7517@naver.com>
2026-07-23 21:38:21 +0900

    프로그래머스 불량사용자 비트마스킹 풀이

--- src/main/java/Programmers/Level_3/Solution_불량사용자.java
+++ src/main/java/Programmers/Level_3/Solution_불량사용자.java
@@ -3,56 +3,51 @@ package Programmers.Level_3;
 import java.util.*;
 
 public class Solution_불량사용자 {
-    static boolean[] visited;
-    static String[] answer;
-    static Set<Set<String>> resultSet;
+    static Set<Integer> combinations;   // 제재 명단에 비트마스킹 적용, 비트마스킹 된 값 Integer
 
+    // 사용자 아이디 중 일부를 * 문자로 가림
+
+    // user_id - 응모자 목록
+    // banned_id - 불량 사용자 목록
     public int solution(String[] user_id, String[] banned_id) {
-        visited = new boolean[user_id.length];
-        answer = new String[banned_id.length];  // result Arr는 banned_id 길이만큼
-        resultSet = new HashSet<>();
+        combinations = new HashSet<>();
 
-        backtracking(0, user_id, banned_id);
+        int mask = 0;   // 아무도 마스킹 하지 않음. (1 << 0 으로 하면, user_id[0]을 이미 마스킹한걸로 처리)
+        dfs(user_id, banned_id, 0, mask);
 
-        return resultSet.size();
+        return combinations.size(); // 몇 가지 경우의 수? return
     }
 
-
-    static void backtracking(int depth, String[] user_id, String[] banned_id) {
-        // depth는 user_id 길이만큼 돌면, 모두 확인한 것이므로 종료
-        if(depth == banned_id.length) {
-//             for(int i = 0; i < answer.length; i++) {
-//                 System.out.print(answer[i] + " ");
-//             }
-//             System.out.println();
-
-            // frodo crodo abc123
-            // crodo frodo abc123
-            // 순서만 다르고 요소가 같은 경우도 중복으로 취급.
-            // 순서가 없는 Set의 성질을 이용하자..
-            Set<String> tempSet = new HashSet<>(Arrays.asList(answer));
-
-            resultSet.add(tempSet);
+    static void dfs(String[] user_id, String[] banned_id, int depth, int mask) {
+        // 제재 명단을 다 확인했다면
+        if( depth == banned_id.length ) {
+            combinations.add(mask);
             return;
         }
 
+        String banned = banned_id[depth];
+
         for(int i = 0; i < user_id.length; i++) {
-            if( visited[i] ) continue;
-            if( user_id[i].length() != banned_id[depth].length() ) continue;    // 길이가 안 맞는 유저 아이디는 skip
-            if( !isBanned( user_id[i], banned_id[depth] )) continue;    // Ban되는 사용자가 아니면 skip
+            // 현재 유저 user_id[i]가 이미 마스킹 되었나?
+            if( ( mask & (1 << i) ) != 0 ) continue;
 
-            visited[i] = true;
-            answer[depth] = user_id[i];
+            // 제재 아이디 패턴과 동일한가?
+            if( !isBanned(banned, user_id[i] ) ) continue;
 
-            backtracking(depth + 1, user_id, banned_id);
-            visited[i] = false;
+            int nextMask = mask | (1 << i);
+
+            dfs(user_id, banned_id, depth + 1, nextMask);
         }
     }
 
-    static boolean isBanned(String userId, String bannedId) {
-        for(int i = 0; i < userId.length(); i++) {
-            if( bannedId.charAt(i) == '*' ) continue;
-            if( userId.charAt(i) != bannedId.charAt(i) ) return false;
+    static boolean isBanned(String banned, String userId) {
+        // 1. 일단 둘의 길이가 맞아야 하고
+        if(banned.length() != userId.length()) return false;
+
+        // 2. 인덱스 위치에 있는 문자가 서로 같아야 하거나, *로 되어있어야함.
+        for(int i = 0; i < banned.length(); i++) {
+            // * 문자열이 아닌데, 두 문자열의 같은 위치에 글자가 다르면
+            if( banned.charAt(i) != '*' && banned.charAt(i) != userId.charAt(i) ) return false;
         }
 
         return true;
