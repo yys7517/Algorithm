52496552504a7f50a2da12ed4490bc8fef8f3ab1
yys7517 <yys7517@naver.com>
2026-07-23 16:15:38 +0900

    프로그래머스 도둑질 복습

--- src/main/java/Programmers/Level_3/Solution_도둑질.java
+++ src/main/java/Programmers/Level_3/Solution_도둑질.java
@@ -3,30 +3,32 @@ package Programmers.Level_3;
 public class Solution_도둑질 {
     public int solution(int[] money) {
         int answer = 0;
+        int n = money.length;
 
-        // 첫 번째 집을 터는 기준
-        int[] dp1 = new int[money.length];
-        dp1[0] = money[0];
-        dp1[1] = money[0];
+        // ex) money = [1,10,20,4,40]
 
-        for(int i = 2; i < dp1.length - 1; i++) {
-            dp1[i] = Math.max(dp1[i-2] + money[i] , dp1[i-1]);
+        // 첫 번째 집을 터는 기준, 마지막 집을 못 턴다.
+        // [1,10,20,4,40] -> [1,10,20,4] -> [0,1,10,20,4] ( 0 + money[0] ~ money[n-2] )
+        int[] first = new int[n];
+        first[0] = 0;
+        first[1] = money[0];
+
+        for(int i = 2; i < n; i++) {
+            first[i] = Math.max( first[i-1], first[i-2] + money[i-1] );
         }
 
-        int[] dp2 = new int[money.length];
-        dp2[0] = 0;
-        dp2[1] = money[1];
+        // 마지막 집을 터는 기준, 첫 번째 집을 못 턴다.
+        // [1,10,20,4,40] -> [10,20,4,40] -> [0,10,20,4,40] ( 0 + money[1] ~ money[n-1] )
+        int[] last = new int[n];
+        last[0] = 0;
+        last[1] = money[1];
 
-        for(int i = 2; i < dp2.length; i++) {
-            dp2[i] = Math.max(dp2[i-2] + money[i] , dp2[i-1]);
+        for(int i = 2; i < n; i++) {
+            last[i] = Math.max( last[i-1], last[i-2] + money[i] );
         }
 
-        int max = Integer.MIN_VALUE;
+        answer = Math.max( last[n-1], first[n-1] );
 
-        for(int i = 0; i < dp1.length; i++) {
-            max = Math.max(max, Math.max(dp1[i], dp2[i]));
-        }
-
-        return max;
+        return answer;
     }
 }
