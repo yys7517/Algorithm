2be0fa4c6f2999895d54b4fb817b61d455093329
yys7517 <yys7517@naver.com>
2026-07-23 16:41:40 +0900

    프로그래머스 스티커 모으기(2) 복습

--- src/main/java/Programmers/Level_3/Solution_스티커_모으기.java
+++ src/main/java/Programmers/Level_3/Solution_스티커_모으기.java
@@ -1,30 +1,34 @@
 package Programmers.Level_3;
 
 public class Solution_스티커_모으기 {
-    public int solution(int sticker[]) {
+    public int solution(int[] sticker) {
         int answer = 0;
+        int n = sticker.length;
 
-        // 배열의 길이는 1이상 100_000
-        if(sticker.length == 1) {
-            return sticker[0];
+        // sticker는 원형으로 연결된 스티커의 각 칸에 적힌 숫자가 순서대로 들어있는 배열로, 길이(N)는 1 이상 100,000 이하
+        if( n == 1 ) return sticker[0];
+
+        // 첫 번째 스티커를 사용하는 경우, 마지막 스티커를 사용하지 않는다.
+        // [1, 3, 2, 5, 4] -> [0, 1, 3, 2, 5] ( 0 + sticker[0] ~ sticker[n-2] )
+        int[] first = new int[n];
+        first[0] = 0;
+        first[1] = sticker[0];
+        for(int i = 2; i < n; i++) {
+            first[i] = Math.max( first[i-1], first[i-2] + sticker[i-1] );
         }
 
-        int[] dp1 = new int[sticker.length];
-        dp1[0] = sticker[0];
-        dp1[1] = sticker[0];
+        // 마지막 스티커를 사용하는 경우
+        // [1, 3, 2, 5, 4] -> [0, 3, 2, 5, 4] ( 0 + sticker[1] ~ sticker[n-1] )
+        int[] last = new int[n];
+        last[0] = 0;
+        last[1] = sticker[1];
 
-        for(int i = 2; i < dp1.length - 1; i++) {
-            dp1[i] = Math.max(dp1[i-2] + sticker[i] , dp1[i-1]);
+        for(int i = 2; i < n; i++) {
+            last[i] = Math.max( last[i-1], last[i-2] + sticker[i] );
         }
 
-        int[] dp2 = new int[sticker.length];
-        dp2[0] = 0;
-        dp2[1] = sticker[1];
+        answer = Math.max( last[n-1], first[n-1] );
 
-        for(int i = 2; i < dp2.length; i++) {
-            dp2[i] = Math.max(dp2[i - 2] + sticker[i] , dp2[i - 1]);
-        }
-
-        return Math.max(dp1[dp1.length - 2], dp2[dp2.length - 1]);
+        return answer;
     }
 }
